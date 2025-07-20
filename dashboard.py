d784f0090027cc602bbbc1c9de9673059370c0c5
lateriteperu <rvargas@laterite.com>
2025-07-20 07:22:52 -0500

    Add password

--- dashboard.py
+++ dashboard.py
@@ -12,12 +12,40 @@ st.set_page_config(
     layout="wide"
 )
 
+# Password protection
+def check_password():
+    """Returns `True` if the user has the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["password"] == st.secrets["PASSWORD"]:
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return `True` if the user has already entered the correct password.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state and not st.session_state["password_correct"]:
+        st.error("Password incorrect")
+    return False
+
+if not check_password():
+    st.stop()  # Do not continue if the password is wrong.
+
+
 # Define the overall project target and the lists of villages by type
 OVERALL_TARGET = 120
 CERTIFIED_VILLAGES = [
     'ALTO ANDINO', 'ALTO RIOJA', 'DINAMARCA', 'FLOR DE MAYO',
     'FLOR DE PRIMAVERA', 'GERVACIO', 'HUICUNGO', 'MONTE RICO',
-    'SANTA MARTHA', 'VILLA HERMOSA'
+    'SANTA MARTHA', 'VILLA HERMOSA' , 'EL PARAISO' , 'LA PERLA'
 ]
 PROJECT_VILLAGES = [
     'ALTO SHAMBOYAKU', 'CHUMBAQUIHUI', 'KACHIPAMPA'
